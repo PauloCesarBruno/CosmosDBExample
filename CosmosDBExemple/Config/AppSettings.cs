738908d7af0f835a65529db139f4557f3551195e
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-11-10 19:06:16 -0300

    Add project files.

---
+++ CosmosDBExemple/Config/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace CosmosDBExample.Config
+{
+    public static class AppSettings
+    {
+        public static readonly string CosmosDdEndpointUri = "https://testandocosmosdb.documents.azure.com:443/";
+        public static readonly string CosmosDdPrimaryKey = "bIUUixi0mcv7Hr8lhS3PJ2LTcxDmK73PjC5lTYtBHKfkO5ErcOMkEb3DkieMt8FyqLIgLFpMdPKdACDbJtn1KQ==";
+    }
+}
---
+++ CosmosDBExemple/Controllers/PessoaController.cs
@@ -0,0 +1,41 @@
+using CosmosDBExample.Models;
+using CosmosDBExemple.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CosmosDBExemple.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        private readonly IPessoasService _pessoasService;
+        private readonly ILogger _logger;
+
+        public PessoaController(ILogger<PessoaController> logger, IPessoasService pessoasService)
+        {
+            _pessoasService = pessoasService;
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _pessoasService.GetPessoas();
+            return Ok(result);
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> Get(string name)
+        {
+            var result = await _pessoasService.GetPessoasPorNome(name);
+            return Ok(result);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Add(Pessoa pessoa)
+        {
+            await _pessoasService.AddPessoa(pessoa);
+            return Ok();
+        }
+    }
+}
