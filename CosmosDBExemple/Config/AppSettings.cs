a66a15415d7b81925a0ade5f5d0afef54d0bc320
Paulo Bruno <53666610+PauloCesarBruno@users.noreply.github.com>
2023-12-02 11:42:11 -0300

    Update AppSettings.cs
    Exlusão de ScreyKey

--- CosmosDBExemple/Config/AppSettings.cs
+++ CosmosDBExemple/Config/AppSettings.cs
@@ -5,6 +5,6 @@
     public static class AppSettings
     {
         public static readonly string CosmosDdEndpointUri = "https://localhost:8081";
-        public static readonly string CosmosDdPrimaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        public static readonly string CosmosDdPrimaryKey = "...";
     }
 }
